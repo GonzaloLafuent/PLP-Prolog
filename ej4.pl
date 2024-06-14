cf08b5c877327dea2e6af68fadb5291dd71d63b4
Gonzalo Lafuente <lafuentegonzalo1@gmail.com>
2024-06-14 11:10:24 -0300

    algunas cosas

--- ej4.pl
+++ ej4.pl
@@ -1,3 +1,33 @@
-juntar(X,[],X).
-juntar([],Y,Y).
-juntar([X|Lx],Y, [X|Lz]) :- juntar(lx,y,lz).
+/*
+    4)
+*/
+juntar([],YS,YS).
+juntar([X|XS],Y,[X|ZS]) :- juntar(XS,Y,ZS).
+
+/* 
+    5)
+*/
+ultimo(L,X) :- append(L1,[X],L2).
+
+invertir(L1,L2) :- append(L1,[],[L2|X]).
+
+prefijo(P,L) :- append(P,L2,L).
+
+sufijo(S,L) :- append(L1,S,L).
+
+sublist(S,L) :- prefijo(S,L), sufijo(S,L).
+
+pertenece(X,L) :- append(L1,[X|L2],L).
+
+/*
+    6)
+*/
+
+aplanar([],[X|[]]).
+aplanar([X|L1],L) :- aplanar(L1,[X|L2]).
+
+/*
+    7)
+*/
+palindromo(L,L1) :- append(L,L2,L1), reverse(L,L2).
+
