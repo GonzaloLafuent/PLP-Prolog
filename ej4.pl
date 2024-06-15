aaff578e067a09ebd50c4ff4707b5d23f922501d
Gonzalo Lafuente <lafuentegonzalo1@gmail.com>
2024-06-15 18:53:43 -0300

    ejercicios varios de prolog

--- ej4.pl
+++ ej4.pl
@@ -7,33 +7,60 @@ juntar([X|XS],Y,[X|ZS]) :- juntar(XS,Y,ZS).
 /* 
     5)
 */
-ultimo(L,X) :- append(L1,[X],L2).
+ultimo(_,X) :- append(_,[X],_).
 
-invertir(L1,L2) :- append(L1,[],[L2|X]).
+invertir(L1,L2) :- append(L1,[],[L2|_]).
 
-prefijo(P,L) :- append(P,L2,L).
+prefijo(P,L) :- append(P,_,L).
 
-sufijo(S,L) :- append(L1,S,L).
+sufijo(S,L) :- append(_,S,L).
 
 sublist(S,L) :- prefijo(S,L), sufijo(S,L).
 
-pertenece(X,L) :- append(L1,[X|L2],L).
+pertenece(X,L) :- append(_,[X|_],L).
 
 /*
     6)
+    aplanar([],[X|[]]).
+    aplanar([X|L1],L) :- aplanar(L1,[X|L2]).
 */
-
-aplanar([],[X|[]]).
-aplanar([X|L1],L) :- aplanar(L1,[X|L2]).
 
 /*
     7)
 */
 palindromo(L,L1) :- append(L,L2,L1), reverse(L,L2).
 
+
+iesimo(I,L,X) :- append(L1,[X|_],L), N is I-1 , length(L1,N).
+
 /*
     8)
 */
+interseccion([],_,[]).
+interseccion([X|XS],L2,[X|L3]) :- interseccion(XS,L2,L3), member(X,L2).
+interseccion([_|XS],L2,L3) :- interseccion(XS,L2,L3).
 
-interseccion([],L2,[]).
-interseccion([X|XS],L2,L3) :- member(X,L2), append(XS,L2,L).
+partir(N,L,L1,L2) :- append(L,L2,L1), length(L,N).
+
+borrar([],_,[]).
+borrar([X|XS],X,LN) :- borrar(XS,X,LN).
+borrar([Y|XS],X,[Y|LN]) :- X\=Y, borrar(XS,X,LN).
+
+sacarDuplicados([],[]).
+sacarDuplicados([X|L1],[X|L2]) :- borrar(L1,X,L), sacarDuplicados(L,L2).
+
+/*
+permutacion([X|XS],L) :- permutacion(XS,L1), inter
+*/
+
+/*
+    9)
+*/
+
+elementosTomadosEnOrden(_,0,[]).
+elementosTomadosEnOrden([X,Y|L1],N,[X|L2]) :- N1 is N-1 ,elementosTomadosEnOrden([Y,L1],N1,L2).
+elementosTomadosEnOrden([X|L1],N,[X|L2]) :-  N1 is N-1, elementosTomadosEnOrden(L1,N1,L2).
+
+/*
+  10)
+*/
