0ef2a808f0a5701e7e5cd542b6a20966c585475d
Gonzalo Lafuente <lafuentegonzalo1@gmail.com>
2024-06-19 16:27:25 -0300

    arbol

--- ej4.pl
+++ ej4.pl
@@ -49,6 +49,8 @@ borrar([Y|XS],X,[Y|LN]) :- X\=Y, borrar(XS,X,LN).
 sacarDuplicados([],[]).
 sacarDuplicados([X|L1],[X|L2]) :- borrar(L1,X,L), sacarDuplicados(L,L2).
 
+reparto(,0,LL).
+reparto(L,N,LL) :- 
 /*
 permutacion([X|XS],L) :- permutacion(XS,L1), inter
 */
@@ -63,4 +65,65 @@ elementosTomadosEnOrden([X|L1],N,[X|L2]) :-  N1 is N-1, elementosTomadosEnOrden(
 
 /*
   10)
+  Para evitar que se cicle se deberia evitar que X sea mayor a Y.
+  Esto se debe a que nunca va entra en la primer regla definida, luego
+  al entrar siempre por la segunda va tener a aumentar X sin producir ninfun resultado
+  si llamo desde con x = y, luego va ser siempre true tal que entro por la primer instruccion
+  si x < y, luego lo voy a aumentar hasta llegar a x, por lo tanto voy a volver entrar
+  por la primer instruccion dando true. si no insstancia Y me va dar X, lo mismo al reves.
 */
+desde(X,X).
+desde(X,Y) :- N is X+1, desde(N,Y).
+
+desde2(X,Y) :- Y is X+1.
+desde2(X,Y) :- N is X+1, desde2(N,Y).
+
+/*
+    11)
+*/
+
+intercalar([],L2,L2).
+intercalar(L1,[],L1).
+intercalar([X|L1],[Y|L2],[X,Y|L3]) :- intercalar(L1,L2,L3).
+
+/*
+    12)
+*/
+vacio(nil).
+
+raiz(bin(_,R,_),RAIZ) :- RAIZ is R.
+
+cantidadDeNodos(nil,N) :- N is 0.
+cantidadDeNodos(bin(I,_,D),N) :- cantidadDeNodos(I,N1), cantidadDeNodos(D,N2), N is N1 + N2 +1. 
+
+altura().
+
+/*
+    13)
+*/
+
+inorder(nil,[]).
+inorder(bin(I,R,D),L) :- inorder(I,LI), inorder(D,LD), append(LI,[R|LD],L).
+
+/*
+arbolConInorder([],nil).
+arbolConInorder([I,_,D|L],).
+*/
+
+aBB(nil).
+aBB(bin(nil,_,nil)).
+aBB(bin(I,R,D)) :- raiz(I,IR), raiz(D,DR), IR < R, DR > R,aBB(I), aBB(D).
+
+/*
+    14)
+*/
+
+ 
+coprimos(X,Y) :- desde2(1,X), desde2(1,Y), gcd(X,Y) == 1.
+
+
+
+
+
+
+
